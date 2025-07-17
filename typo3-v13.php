a6caadf71e1cc643380881ad1825a34e7d0b6b58
Mike Street <mikestreety@gmail.com>
2025-07-17 10:22:25 +0100

    feat: Add typo3-v13 config file

---
+++ typo3-v13.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use LintKit\Typo3RectorConfig\Set\LintKitSetList;
+
+return RectorConfig::configure()
+	->withSets([
+		LintKitSetList::UP_TO_TYPO3_13,
+	])
+;
